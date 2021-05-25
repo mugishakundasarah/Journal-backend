5246b4b46d680b9b8a2795f93e51861415943f6b
spfvtnjr <74837665+spfvtnjr@users.noreply.github.com>
2021-05-25 06:21:52 -1200

    index update

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require("express")
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
-// const port = require("./config/keys").port;
 const app = express()
 require('./models/db')
-// const {postRoutes}=require('./routes/posts.routes')
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-// app.use('/api/posts', postRoutes)
 app.use("/", (req, res) => {
     res.send(formatResult({
         status: 200,
