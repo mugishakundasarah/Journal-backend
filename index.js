0c85ceef9014076695f2dd8f53dcb7e6e4afdb63
spfvtnjr <74837665+spfvtnjr@users.noreply.github.com>
2021-05-24 21:29:03 -1200

    added swagger documentation

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const express = require('express');
-const app = express();
-const port = 5400;
+const express = require("express")
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+// const port = require("./config/keys").port;
+const app = express()
+require('./models/db')
+// const {postRoutes}=require('./routes/posts.routes')
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use('/api/posts', postRoutes)
+app.use("/", (req, res) => {
+    res.send(formatResult({
+        status: 200,
+        message: 'welcome to our page'
+    }));
+});
+port=5000;
+app.listen(port, () => console.log(`Listening on port ${port}..`));
 
-require("./models/db");
 
-app.listen(port)
