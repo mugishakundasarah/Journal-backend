74b9e5de3783ec54104ae4fbdc8c38d045afbf4e
graziee6 <gngrace10@gmail.com>
2021-05-25 15:23:54 +0200

    Added joi validation to client and note models

--- models/db.js
+++ models/db.js
@@ -3,7 +3,7 @@ const debug = require("debug")("app:database")
 
 const connect = async() => {
     try {
-        await mongoose.connect("mongodb+srv://sarah:28!01!2020@cluster0.jldhx.mongodb.net/journal?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});    
+        await mongoose.connect("mongodb+srv://sarah:28!01!2020@cluster0.jldhx.mongodb.net/journal?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});  
         debug("successfully connected");
     } catch (error) {
         debug(error)
