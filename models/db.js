0b04be8fa561cb276d87714f7b6559d19180c8cf
Christian-pprogrammer <mpanoc6@gmail.com>
2021-05-27 16:26:37 +0200

    random message

--- models/db.js
+++ models/db.js
@@ -1,25 +1,25 @@
 const mongoose = require("mongoose")
 const debug = require("debug")("app:database")
 
-// const connect = async() => {
-//     try {
-//         await mongoose.connect("mongodb://localhost/journal", {useNewUrlParser: true, useUnifiedTopology: true});    
-//         debug("successfully connected");
-//     } catch (error) {
-//         debug(error)
-//     }
-// }
-async function connect(){
-    mongoose.connect("mongodb://localhost/journal",
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true 
-    })
-    .then(()=>{
-        console.log("connected")
-    }).catch(err=>{
-        console.log(err);
-    })
+const connect = async() => {
+    try {
+        await mongoose.connect("mongodb://localhost/journal", {useNewUrlParser: true, useUnifiedTopology: true});    
+        debug("successfully connected");
+    } catch (error) {
+        debug(error)
+    }
 }
+// async function connect(){
+//     mongoose.connect("mongodb://localhost/journal",
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true 
+//     })
+//     .then(()=>{
+//         console.log("connected")
+//     }).catch(err=>{
+//         console.log(err);
+//     })
+// }
 
 connect();
