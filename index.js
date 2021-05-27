c5a22d47ea0e18139aa97a1f0b116fa5068ea689
mugishakundasarah <mugishakundasarah@gmail.com>
2021-05-27 14:53:41 +0200

    resolved the modules problem

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const config = require("config")
 const port = config.get("app.port");
 const appDebug = require("debug")("app:Debug")
 const express = require("express")
-const swaggerUi = require('swagger-ui-express');
+const swaggerUi = require("swagger-ui-express")
 const swaggerDocument = require('./swagger.json');
 const app = express()
+const {formatResult} = require("./utils/import")
 require('./models/db')
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
